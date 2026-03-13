9f65e00841bd648966d3197ecb12f6986658319d
Taras Oleynik <taras.oleynik@gmail.com>
2026-03-13 17:22:06 +0200

    fixxed to 3 decm

--- src/main.js
+++ src/main.js
@@ -13,8 +13,7 @@ const volumeResult = document.querySelector("#volume-result");
 const massResult = document.querySelector("#mass-result");
 
 convertBtn.addEventListener("click", () => {
-  lengthResult.innerHTML = `${inputNumber.value} meters = ${inputNumber.value * 3.281} feet | ${inputNumber.value} feet = ${(inputNumber.value / 3.282).toFixed(3)} meters`;
+  lengthResult.innerHTML = `${inputNumber.value} meters = ${(inputNumber.value * 3.281).toFixed(3)} feet | ${inputNumber.value} feet = ${(inputNumber.value / 3.282).toFixed(3)} meters`;
   volumeResult.innerHTML = `${inputNumber.value} liters = ${(inputNumber.value * 0.264).toFixed(3)} gallons | ${inputNumber.value} gallons = ${(inputNumber.value / 0.264).toFixed(3)} liters`;
   massResult.innerHTML = `${inputNumber.value}  kilos = ${(inputNumber.value * 2.204).toFixed(3)}  pounds | ${inputNumber.value}  pounds = ${(inputNumber.value / 2.204).toFixed(3)} kilos`;
-  console.log(typeof (inputNumber.value * 3.281));
 });
