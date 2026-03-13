85e69cfe14dd1b4592f475fc5fdfd1f96da90608
Taras Oleynik <taras.oleynik@gmail.com>
2026-03-13 17:18:54 +0200

    update converter

--- src/main.js
+++ src/main.js
@@ -9,8 +9,12 @@
 const lengthResult = document.querySelector("#length-result");
 const convertBtn = document.querySelector("#convert-btn");
 const inputNumber = document.querySelector("#input-number");
+const volumeResult = document.querySelector("#volume-result");
+const massResult = document.querySelector("#mass-result");
 
 convertBtn.addEventListener("click", () => {
-  lengthResult.innerHTML = `${inputNumber.value} meters = ${inputNumber.value * 3.281} feet | ${inputNumber.value} feet = ${(inputNumber.value / 3.282).toFixed(3)} meters`; // inputNumber.value;
+  lengthResult.innerHTML = `${inputNumber.value} meters = ${inputNumber.value * 3.281} feet | ${inputNumber.value} feet = ${(inputNumber.value / 3.282).toFixed(3)} meters`;
+  volumeResult.innerHTML = `${inputNumber.value} liters = ${(inputNumber.value * 0.264).toFixed(3)} gallons | ${inputNumber.value} gallons = ${(inputNumber.value / 0.264).toFixed(3)} liters`;
+  massResult.innerHTML = `${inputNumber.value}  kilos = ${(inputNumber.value * 2.204).toFixed(3)}  pounds | ${inputNumber.value}  pounds = ${(inputNumber.value / 2.204).toFixed(3)} kilos`;
   console.log(typeof (inputNumber.value * 3.281));
 });
